dfcf7091a765f74791c98b7b670a3320c9fafda0
pabloTokuo <pbo_1996@hotmail.com>
2023-10-13 18:15:01 -0300

    verificacao de idade feita e css de esconder/aparecer erro adicionado

--- js/index.js
+++ js/index.js
@@ -20,27 +20,97 @@ const onlyNumber = (v) => {
 const btn = document.querySelector('[data-js="btn"]');
 const dayInput = document.getElementById('day');
 const monthInput = document.getElementById('month');
-const year = document.getElementById('year');
-
+const yearInput = document.getElementById('year');
 
 btn.addEventListener('click', () => {
   let dayValue = parseInt(dayInput.value);
   let monthValue = parseInt(monthInput.value);
+  let yearValue = parseInt(yearInput.value);
   
 
-  const isDayValidForMonth = (day, month) => {
-    // Validating month
+  const isDayValidForMonth = (day, month, year) => {
     if (month < 1 || month > 12) {
       return false;
     }
+
+    if (year > new Date().getFullYear()) {
+      return false;
+    }
   
-    // Creating Date object
     const date = new Date(new Date().getFullYear(), month - 1, day);
     
     return date.getMonth() === month - 1 && date.getDate() === day;
   }
   
-  const isValid = isDayValidForMonth(dayValue, monthValue);
-  console.log(`Is ${dayValue}/${monthValue} a valid date? ${isValid}`);
+  const isValid = isDayValidForMonth(dayValue, monthValue, yearValue);
+
+  const dayError = document.querySelector(".day-error");
+  const monthError = document.querySelector(".month-error");
+  const yearError = document.querySelector(".year-error");
+  const dayReq = document.querySelector(".day-req");
+  const monthReq = document.querySelector(".month-req");
+  const yearReq = document.querySelector(".year-req");
+
+
+  if(isNaN(dayValue)) {
+    dayReq.style.display = "block";
+  } else {
+    dayReq.style.display = "none";
+  }
+
+  if(isNaN(monthValue)) {
+    monthReq.style.display = "block";
+  } else {
+    monthReq.style.display = "none";
+  }
+
+  if(isNaN(yearValue)) {
+    yearReq.style.display = "block";
+  } else {
+    yearReq.style.display = "none";
+  }
+
+  if(dayValue == 0 || dayValue > 31) {
+    dayError.style.display = "block";
+  } else {
+    dayError.style.display = "none";
+  }
+
+  if(monthValue == 0 || monthValue > 12) {
+    monthError.style.display = "block";
+  } else {
+    monthError.style.display = "none";
+  }
+
+  if(yearValue > 2023) {
+    yearError.style.display = "block";
+  } else {
+    yearError.style.display = "none";
+  }
+
+  const oneDay = 24 * 60 * 60 * 1000;
+
+  const spanYear = document.querySelector("[data-year='year']");
+  const spanMonth = document.querySelector("[data-month='month']");
+  const spanDay = document.querySelector("[data-day='day']");
+
+  let totalMonth = monthValue - (new Date().getMonth() + 1);
+  spanMonth.innerText = totalMonth;
+
+  const birthdate = new Date(`${yearValue}-${monthValue}-${dayValue}`);
+  const today = new Date();
+
+  // Calculate the age difference
+  const ageDiff = today - birthdate;
+
+  spanYear.innerText = Math.floor(ageDiff / (oneDay * 365));
+  spanMonth.innerText = Math.floor((ageDiff % (oneDay * 365)) / (oneDay * 30));
+  spanDay.innerText = Math.floor(((ageDiff % (oneDay * 365)) % (oneDay * 30)) / oneDay);
+
+  if(isNaN(yearValue) || isNaN(monthValue) || isNaN(dayValue)) {
+    spanYear.innerText = '- -';
+    spanMonth.innerText = '- -';
+    spanDay.innerText = '- -';
+  }
 })
 
